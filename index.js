047114bf7064f4f1eeeccc98ec83a55435e2c99a
MusicalWhiskey <124202980+MusicalWhiskey@users.noreply.github.com>
2024-11-19 13:08:01 -0700

    port

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ app.use("/scores", scoresRoutes);
 dotenv.config();
 connect();
 
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 // Routes
 app.use('/api/scores', scoresRoutes);
